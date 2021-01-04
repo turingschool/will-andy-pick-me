27a45986db0ec631e64ebcfd79668dd305466d36
Elizabeth Hahn <elizhahn87@gmail.com>
2021-01-04 11:53:10 -0700

    Query select the clear button, begin clear function

--- index.js
+++ index.js
@@ -4,15 +4,23 @@ var textQuestion = document.querySelector(".question");
 var textAnswer = document.querySelector(".answer");
 var inputQuestion = document.querySelector(".input-question");
 var btnGetAnswer = document.querySelector(".button-get-answer");
+var btnClear = document.querySelector(".button-clear");
 var img8Ball = document.querySelector(".img-magic-8-ball");
 
-
 btnGetAnswer.addEventListener("click", getAnswer);
+// btnClear.addEventListener("click",);
+
 
 function displayAnswer() {
   img8Ball.classList.add("hidden");
   textQuestion.classList.remove("hidden");
   textAnswer.classList.remove("hidden");
+}
+
+function clearDisplay() {
+  img8Ball.classList.remove("hidden");
+  textQuestion.classList.add("hidden");
+  textAnswer.classList.add("hidden");
 }
 
 function randomAnswer() {
@@ -27,4 +35,10 @@ function getAnswer(e) {
   displayAnswer();
   textQuestion.innerText = `"${inputQuestion.value}?"`
   textAnswer.innerText = `${randomAnswer()}`
+  btnClear.classList.remove("disabled");
+}
+
+function clearAnswer() {
+  clearDisplay();
+
 }
